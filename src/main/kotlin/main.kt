ead5f9fddb1b46a4932539a8eaf0e40e3ca5c1e5
bgchoi205 <choibg205@naver.com>
2021-04-14 20:44:28 +0900

    명령어입력 구현

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,14 @@
 fun main() {
-    print("hi")
+    println("안녕하세요")
+    println("==프로그램 시작==")
+
+    while(true){
+        print("명령어 입력 > ")
+        val command = readLine()
+
+        println("입력한 명령어 : $command")
+
+    }
+
+    print("==프로그램 종료==")
 }
